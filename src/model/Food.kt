6bba380a9096ac2be43c2312f797073cd30e0c3e
noor serry <noor.serry.94@gmail.com>
2025-04-15 16:14:57 +0200

    move Nutrition data class to different file

--- src/model/Food.kt
+++ src/model/Food.kt
@@ -14,13 +14,4 @@ data class Food(
     val ingredients: List<String>
 )
 
-data class Nutrition(
-    val calories: Float,
-    val totalFat: Float,
-    val sugar: Float,
-    val sodium: Float,
-    val protein: Float,
-    val saturated: Float,
-    val carbohydrates: Float
 
-)
