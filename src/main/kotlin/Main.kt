2fc6e9167c73b396df650bfaec493b9b19053f5b
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-16 13:34:47 +0200

    remove unused imports

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
-import data.FakeCsvFoodRepositoryImp
 import di.appModule
 import di.useCaseModule
-import logic.repository.FoodRepository
-import logic.usecase.GetMealForThinPeopleUseCase
 import org.koin.core.context.startKoin
 import org.koin.mp.KoinPlatform.getKoin
 import presentation.FoodChangeMoodConsoleUI
