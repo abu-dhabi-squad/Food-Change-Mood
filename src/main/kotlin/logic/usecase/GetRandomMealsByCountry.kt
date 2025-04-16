dac4f02ac0bca55364efb6e03011163e56ad7fa6
Mohamed Emad <mohamedemad199912@gmail.com>
2025-04-16 22:20:31 +0200

    enhance get random meals use case by make it search for any related column

--- src/main/kotlin/logic/usecase/GetRandomMealsByCountry.kt
+++ src/main/kotlin/logic/usecase/GetRandomMealsByCountry.kt
@@ -2,25 +2,32 @@ package logic.usecase
 
 import logic.repository.FoodRepository
 import model.CountryMealsFetchException
+import model.Food
 
 class GetRandomMealsByCountry(
     private val foodRepository: FoodRepository
-){
+) {
 
-    fun getRandomMeals(country: String) : List<String>{
+    fun getRandomMeals(country: String): List<String> {
 
         return foodRepository
             .getFoods()
             .getOrThrow()
-            .filter { meal ->
-                meal.tags.any { tag ->
-                    tag.contains(country, ignoreCase = true)
-                } == true
+            .filter { meal -> meal.matchesCountry(country)
             }.takeIf { meals -> meals.isNotEmpty() }
             ?.mapNotNull { meal -> meal.name }
             ?.shuffled()
             ?.take(20)
+            ?.sorted()
             ?: throw CountryMealsFetchException(message = "Failed to fetch meals for the given country")
 
     }
+
+    private fun Food.matchesCountry(country: String): Boolean {
+
+        return tags.any { tag -> tag.contains(country.lowercase()) } == true
+                || name?.lowercase()?.contains(country.lowercase()) == true
+                || description?.lowercase()?.contains(country.lowercase()) == true
+    }
+
 }
