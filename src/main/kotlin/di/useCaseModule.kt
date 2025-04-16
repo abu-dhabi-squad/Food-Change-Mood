2a7d87e80118321cfb8c7dc352d27aa70e2079c9
noor serry <noor.serry.94@gmail.com>
2025-04-16 21:28:10 +0200

    add use cases to useCaseModule for dependency injection

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,7 +1,10 @@
 package di
 
+import logic.usecase.GetRandomFoodUseCase
+import logic.usecase.GuessFoodPreparationTimeUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module{
-
+    val getRandomFoodUseCase = single { GetRandomFoodUseCase(get()) }
+    val guessFoodPreparationTimeUseCase = single { GuessFoodPreparationTimeUseCase() }
 }
