ae4d2ed4d6182583e8c2f686016a32e967e3764c
Elsayed Magdy <elsayed3690@gmail.com>
2025-04-16 15:09:14 +0200

    add get random potatoes meals use case

---
+++ src/main/kotlin/logic/usecase/GetRandomPotatoMealsUseCase.kt
@@ -0,0 +1,41 @@
+package logic.usecase
+
+import logic.repository.FoodRepository
+
+
+class GetRandomPotatoMealsUseCase(
+    private val foodRepository: FoodRepository
+) {
+
+    fun getTenRandomPotatoMeals(): List<String> {
+        return try {
+            foodRepository
+                .getFoods()
+                .fold(
+                    onFailure = {
+                        throw Exception("error in getting meals")
+                    },
+                    onSuccess = { meals ->
+                         meals
+                            .filter {
+                                !it.name.isNullOrBlank() && it.ingredients.any {
+                                    it.contains(
+                                        POTATOES_KEYWORD,
+                                        ignoreCase = true
+                                    )
+                                }
+                            }
+                            .shuffled()
+                            .take(MEALS_COUNT)
+                            .map { it.name.toString() }
+                    })
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    companion object {
+        const val MEALS_COUNT = 10
+        const val POTATOES_KEYWORD = "potatoes"
+    }
+}
