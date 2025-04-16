76841d6decf423e800a699522ecc847e96456cb9
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-16 13:19:06 +0200

    create get meal for thin people use case

---
+++ src/main/kotlin/logic/usecase/GetMealForThinPeopleUseCase.kt
@@ -0,0 +1,33 @@
+package logic.usecase
+
+import logic.repository.FoodRepository
+import model.Food
+
+class GetMealForThinPeopleUseCase(
+    private val foodRepository: FoodRepository
+)
+{
+    private var mealIndex = 0
+    fun getMeal():Food{
+        foodRepository.getFoods()
+            .fold(
+                onSuccess = {
+                    meals ->
+                    return meals.filter(::onlyHighCaloriesData)
+                        .sortedByDescending { it.nutrition.calories }
+                        .takeIf { it.isNotEmpty() && it.size > mealIndex}
+                        ?.get(mealIndex)
+                        ?: throw Exception("there is no high calories meals in list")
+                            },
+                onFailure = {throw Exception("there is no food in list")},
+            )
+    }
+
+    private fun onlyHighCaloriesData(food: Food):Boolean{
+        return food.name != null && food.description != null && food.nutrition.calories > 700.0
+    }
+
+    fun dislikeTheCurrentMeal(){
+        mealIndex++
+    }
+}
