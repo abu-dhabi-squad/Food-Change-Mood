d54c41261fc29176b13365f58afc0bbb22b3f614
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-16 22:44:25 +0200

    create search meals by date use case

---
+++ src/main/kotlin/logic/usecase/GetFoodByDateUseCase.kt
@@ -0,0 +1,28 @@
+package logic.usecase
+
+import logic.repository.FoodRepository
+import model.EmptySearchByDateListException
+import model.Food
+import java.time.LocalDate
+
+
+class GetFoodByDateUseCase(
+    private val foodRepository: FoodRepository
+) {
+    fun getMealsByDate(date: LocalDate): List<Pair<Int,String>>{
+        return foodRepository.getFoods()
+            .getOrThrow()
+            .filter { IsItTheWantedDate(it,date) }
+            .takeIf { it.isNotEmpty() }
+            ?.map { it.id to it.name!! }
+            ?: throw EmptySearchByDateListException()
+    }
+    fun getMealById(id: Int): Food {
+        return foodRepository.getFoods()
+            .getOrThrow()
+            .find{it.id == id}!!
+    }
+    private fun IsItTheWantedDate(food: Food, date: LocalDate):Boolean{
+        return food.name != null && food.description != null && food.submittedDate == date
+    }
+}
--- src/main/kotlin/model/exceptions.kt
+++ src/main/kotlin/model/exceptions.kt
@@ -1,2 +1,11 @@
 package model
 
+open class AppException(message: String): Exception(message)
+
+class EmptyListException:AppException("there is no item in list")
+
+class EmptyHighCalorieListException:AppException("there is no high calories meals in list")
+
+class EmptySearchByDateListException:AppException("there is no meals in this date list")
+
+class WrongInputException:AppException("wrong Input")
