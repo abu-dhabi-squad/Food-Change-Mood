4480d4ec14ab12d7ceb5cd690758479eb3489883
Eslam <eslam.mohamed.551999@gmail.com>
2025-04-16 20:11:20 +0200

    refactor gym helper use case

--- src/main/kotlin/logic/usecase/GymHelperUseCase.kt
+++ src/main/kotlin/logic/usecase/GymHelperUseCase.kt
@@ -9,25 +9,24 @@ class GymHelperUseCase(
     private val foodRepository: FoodRepository
 ) {
     fun getListOfMealsForGym(calories: Float, protein: Float): List<Food> {
-        return foodRepository.getFoods().fold(
-            onSuccess = { meals ->
-                meals.filter { meal -> isSuitableMeal(meal, calories, protein) }
-                    .takeIf { foods: List<Food> -> foods.isNotEmpty() }
-                    ?.sortedBy { food: Food -> calculateMealScore(food, calories, protein) }
-                    ?: throw NoMealsFoundException()
-            },
-            onFailure = { throw it },
-        )
+        return foodRepository.getFoods().getOrThrow()
+            .filter { meal -> isSuitableMeal(meal, calories, protein) }
+            .takeIf { foods: List<Food> -> foods.isNotEmpty() }
+            ?.sortedBy { food: Food -> calculateMealScore(food, calories, protein) }
+            ?: throw NoMealsFoundException()
     }
 
     private fun isSuitableMeal(meal: Food, calories: Float, protein: Float): Boolean {
-        return ((meal.nutrition.calories - calories).absoluteValue / calories) <= ACCEPTABLE_DIFFERENCE_PERCENTAGE
-                && ((meal.nutrition.protein - protein).absoluteValue / protein) <= ACCEPTABLE_DIFFERENCE_PERCENTAGE
+        return getPercentage(calories, meal.nutrition.calories) <= ACCEPTABLE_DIFFERENCE_PERCENTAGE
+                && (getPercentage(protein, meal.nutrition.protein)) <= ACCEPTABLE_DIFFERENCE_PERCENTAGE
     }
 
     private fun calculateMealScore(meal: Food, calories: Float, protein: Float): Float {
-        return (((meal.nutrition.calories - calories).absoluteValue / calories)
-         + ((meal.nutrition.protein - protein).absoluteValue / protein)) / 2
+        return (getPercentage(calories, meal.nutrition.calories) + (getPercentage(protein, meal.nutrition.protein))) / 2
+    }
+
+    private fun getPercentage(baseNum: Float, otherNum: Float): Float {
+        return (baseNum - otherNum).absoluteValue / baseNum
     }
 
     companion object {
