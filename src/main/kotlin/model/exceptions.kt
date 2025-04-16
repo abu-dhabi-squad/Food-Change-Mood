0aa129c71752280fed64dfe16429225e8e65d518
noor serry <noor.serry.94@gmail.com>
2025-04-16 21:23:20 +0200

    add exceptions classes

--- src/main/kotlin/model/exceptions.kt
+++ src/main/kotlin/model/exceptions.kt
@@ -1,2 +1,11 @@
 package model
 
+class NoMealFoundException (exceptionMessage : String?):Exception(exceptionMessage)
+
+open class GuessFoodPreparationTimeException (exceptionMessage : String?):Exception(exceptionMessage)
+
+class RichMaxAttemptException (exceptionMessage : String?):GuessFoodPreparationTimeException(exceptionMessage)
+
+class GuessWrongPreparationTimeException (exceptionMessage : String?):GuessFoodPreparationTimeException(exceptionMessage)
+
+class InValidPreparationTimeException (exceptionMessage : String?):GuessFoodPreparationTimeException(exceptionMessage)
