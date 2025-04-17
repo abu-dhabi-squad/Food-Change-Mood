81ff6d122acff9638ce575667841b3ee6eeb6116
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-04-17 12:15:13 +0300

    add EasyFoodSuggestionGameUseCase(get()) to dependency

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -2,8 +2,11 @@ package di
 
 import data.FakeCsvFoodRepositoryImp
 import logic.repository.FoodRepository
+import logic.usecase.EasyFoodSuggestionGameUseCase
 import org.koin.dsl.module
 
 val appModule = module {
     single<FoodRepository> { FakeCsvFoodRepositoryImp() }
+
+    single { EasyFoodSuggestionGameUseCase(get()) }
 }
