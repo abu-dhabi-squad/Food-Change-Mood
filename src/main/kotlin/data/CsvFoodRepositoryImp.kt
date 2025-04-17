9fcd1fc27c1b57be316d62dd8b9d9766b03ab823
noor serry <noor.serry.94@gmail.com>
2025-04-17 15:43:18 +0200

    use FoodCsvParser class in CsvFoodRepositoryImp class

--- src/main/kotlin/data/CsvFoodRepositoryImp.kt
+++ src/main/kotlin/data/CsvFoodRepositoryImp.kt
@@ -4,10 +4,9 @@ import logic.repository.FoodRepository
 import model.Food
 
 class CsvFoodRepositoryImp(
-    private val csvFileReader: CsvFileReader,
     private val foodCsvParser: FoodCsvParser
 ) : FoodRepository {
     override fun getFoods(): Result<List<Food>> {
-        TODO("Not yet implemented")
+        return Result.success(foodCsvParser.parseCsvFileToFoods())
     }
 }
