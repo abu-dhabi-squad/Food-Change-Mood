c3b65da80d43ba106cc8b07f3504a47cecd76576
Alaa <alaakhaled9191@gmail.com>
2025-04-17 03:45:38 +0200

    add use case to italian meals for large group

---
+++ src/main/kotlin/logic/GetItalianMealsForLargeGroup.kt
@@ -0,0 +1,25 @@
+package logic
+
+import logic.repository.FoodRepository
+import model.Food
+
+class GetItalianMealsForLargeGroupUsesCase(
+    private val foodRepository : FoodRepository
+) {
+
+    fun getItalianMealForLargeGroup(): List<String?> {
+
+        return foodRepository.getFoods().getOrThrow()
+            .filter(::onlyHighQualityData)
+            .filter(::onlyItalianMealForLargeGroup)
+            .map { it.name }
+    }
+
+    private fun onlyItalianMealForLargeGroup(food: Food): Boolean{
+        return (food.tags.contains("italian") && food.tags.contains("for-large-groups"))
+    }
+
+    private fun onlyHighQualityData(input: Food): Boolean{
+        return input.name != null && input.description != null
+    }
+}
