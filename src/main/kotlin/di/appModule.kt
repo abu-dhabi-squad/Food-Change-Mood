de3f2ce8049160d9753bff47b8e21c09466f8256
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-04-17 13:03:09 +0300

    add MealSearchByNameUseCase dependency

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -2,8 +2,10 @@ package di
 
 import data.FakeCsvFoodRepositoryImp
 import logic.repository.FoodRepository
+import logic.usecase.MealSearchByNameUseCase
 import org.koin.dsl.module
 
 val appModule = module {
     single<FoodRepository> { FakeCsvFoodRepositoryImp() }
+    single { MealSearchByNameUseCase(get()) }
 }
