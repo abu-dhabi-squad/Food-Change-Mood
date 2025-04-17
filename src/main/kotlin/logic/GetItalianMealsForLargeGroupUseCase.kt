96d2411e7deb947025469b1dfa07587824294d22
Alaa <alaakhaled9191@gmail.com>
2025-04-17 22:27:42 +0200

    add companion object for italian meals use case

---
+++ src/main/kotlin/logic/GetItalianMealsForLargeGroupUseCase.kt
@@ -0,0 +1,29 @@
+package logic
+
+import logic.repository.FoodRepository
+import model.Food
+
+class GetItalianMealsForLargeGroupUseCase(
+    private val foodRepository : FoodRepository
+) {
+
+    fun getItalianMealForLargeGroup(): List<Pair<String?, String?>> {
+
+        return foodRepository.getFoods().getOrThrow()
+            .filter { onlyHighQualityData(it) && onlyItalianMealForLargeGroup(it) }
+            .map { it.name to it.description }
+    }
+
+    private fun onlyItalianMealForLargeGroup(food: Food): Boolean{
+        return (food.tags.contains(ITALIAN_MEALS) && food.tags.contains(FOR_LARGE_GROUPS))
+    }
+
+    private fun onlyHighQualityData(input: Food): Boolean{
+        return input.name != null && input.description != null
+    }
+
+    companion object{
+        private const val ITALIAN_MEALS = "italian"
+        private const val FOR_LARGE_GROUPS = "for-large-groups"
+    }
+}
