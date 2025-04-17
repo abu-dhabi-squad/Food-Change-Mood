69a07e2126e724673e8aceb07e6d4fcf5e73abfd
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-17 15:13:37 +0200

    add required dependences

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -3,8 +3,14 @@ package di
 import logic.usecase.GetFoodByDateUseCase
 import logic.usecase.GetMealForThinPeopleUseCase
 import org.koin.dsl.module
+import presentation.GetFoodByDateUI
+import util.GetFoodByDateValidationImplementaion
+import util.GetFoodByDateValidationInterface
 
 val useCaseModule = module{
     single { GetMealForThinPeopleUseCase(get()) }
     single { GetFoodByDateUseCase(get()) }
+
+    single<GetFoodByDateValidationInterface> { GetFoodByDateValidationImplementaion() }
+    single { GetFoodByDateUI(get(),get()) }
 }
