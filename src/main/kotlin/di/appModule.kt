d442870f9dee8a601b057b2dfefd1d8cd9813c63
Elsayed Magdy <elsayed3690@gmail.com>
2025-04-17 10:13:50 +0200

    add random potatoes meals console ui in app module

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -3,7 +3,9 @@ package di
 import data.FakeCsvFoodRepositoryImp
 import logic.repository.FoodRepository
 import org.koin.dsl.module
+import presentation.RandomPotatoesMealsConsoleUi
 
 val appModule = module {
     single<FoodRepository> { FakeCsvFoodRepositoryImp() }
+    single{RandomPotatoesMealsConsoleUi(get())}
 }
