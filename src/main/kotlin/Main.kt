8ac4547ac7b113ffb269a4df24bf084e4e8a4173
Elsayed Magdy <elsayed3690@gmail.com>
2025-04-17 10:37:20 +0200

    add random potatoes meals ui

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 import di.appModule
 import di.useCaseModule
 import logic.repository.FoodRepository
+import logic.usecase.GetRandomPotatoesMealsUseCase
 import org.koin.core.context.startKoin
 import org.koin.mp.KoinPlatform.getKoin
 import presentation.FoodChangeMoodConsoleUI
+import presentation.RandomPotatoesMealsConsoleUi
 
 fun main() {
-     startKoin {
-         modules(appModule, useCaseModule)
-     }
+    startKoin {
+        modules(appModule, useCaseModule)
+    }
     val foodRepository = getKoin().get<FoodRepository>()
-    FoodChangeMoodConsoleUI().start()
+    val randomPotatoesMealsConsoleUi = getKoin().get<RandomPotatoesMealsConsoleUi>()
+    FoodChangeMoodConsoleUI(
+        randomPotatoesMealsConsoleUi
+    ).start()
 }
