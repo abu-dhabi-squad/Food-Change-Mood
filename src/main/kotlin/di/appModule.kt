f13a1ee1e4a7d204c9f2113d2c98bc509afa0c89
Eslam <eslam.mohamed.551999@gmail.com>
2025-04-18 01:41:35 +0200

    update di

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -4,8 +4,10 @@ import data.FakeCsvFoodRepositoryImp
 import logic.repository.FoodRepository
 import org.koin.dsl.module
 import presentation.FoodChangeMoodConsoleUI
+import presentation.GymHelperConsoleUI
 
 val appModule = module {
     single<FoodRepository> { FakeCsvFoodRepositoryImp() }
+    single { GymHelperConsoleUI(get()) }
     single { FoodChangeMoodConsoleUI(get(), get()) }
 }
--- src/main/kotlin/di/uiModule.kt
+++
@@ -1,8 +0,0 @@
-package di
-
-import org.koin.dsl.module
-import presentation.GymHelperConsoleUI
-
-val uiModule = module {
-    single { GymHelperConsoleUI(get()) }
-}
