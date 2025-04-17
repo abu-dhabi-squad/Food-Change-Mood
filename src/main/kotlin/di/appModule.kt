0c25b60e98851a71904626334ff7c592e2c9c25e
Alaa <alaakhaled9191@gmail.com>
2025-04-17 05:16:31 +0200

    add food change mood console for ui in app module and use case module

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -3,9 +3,11 @@ package di
 import data.FakeCsvFoodRepositoryImp
 import logic.repository.FoodRepository
 import org.koin.dsl.module
+import presentation.FoodChangeMoodConsoleUI
 import presentation.ItalianMealsForLargeGroupUI
 
 val appModule = module {
     single<FoodRepository> { FakeCsvFoodRepositoryImp() }
+    single { FoodChangeMoodConsoleUI(get()) }
     single { ItalianMealsForLargeGroupUI(get()) }
 }
--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -2,7 +2,9 @@ package di
 
 import logic.GetItalianMealsForLargeGroupUsesCase
 import org.koin.dsl.module
+import presentation.FoodChangeMoodConsoleUI
 
 val useCaseModule = module{
+    single { FoodChangeMoodConsoleUI(get()) }
     single { GetItalianMealsForLargeGroupUsesCase(get()) }
 }
