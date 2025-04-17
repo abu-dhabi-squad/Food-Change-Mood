942f11673a7090e6df8abc93a3062d79eac251fd
Mohamed Emad <mohamedemad199912@gmail.com>
2025-04-17 03:29:22 +0200

    inject dependency

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -3,7 +3,9 @@ package di
 import data.FakeCsvFoodRepositoryImp
 import logic.repository.FoodRepository
 import org.koin.dsl.module
+import presentation.FoodChangeMoodConsoleUI
 
 val appModule = module {
     single<FoodRepository> { FakeCsvFoodRepositoryImp() }
+    single { FoodChangeMoodConsoleUI(get()) }
 }
--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,7 +1,8 @@
 package di
 
+import logic.usecase.GetRandomMealsByCountryUseCase
 import org.koin.dsl.module
 
-val useCaseModule = module{
-
+val useCaseModule = module {
+    single { GetRandomMealsByCountryUseCase(get()) }
 }
