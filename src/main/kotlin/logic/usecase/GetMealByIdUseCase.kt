757822ebf6eeca710ae7c18a461fa3e17733dfce
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-17 20:31:07 +0200

    seperating the get meal by date and get meal by id use cases

---
+++ src/main/kotlin/logic/usecase/GetMealByIdUseCase.kt
@@ -0,0 +1,18 @@
+package logic.usecase
+
+import logic.repository.FoodRepository
+import model.Food
+import model.InvalidIdException
+
+class GetMealByIdUseCase(
+    private val foodRepository: FoodRepository
+)
+{
+    fun getMealById(id: Int): Food {
+        return foodRepository.getFoods()
+            .getOrThrow()
+            .find{it.id == id}
+            .takeIf { it != null }
+            ?: throw InvalidIdException()
+    }
+}
