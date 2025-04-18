9721ce1233da18b8134b317302c5bfd30567e416
Eman Abobakr <eman.abobakr@vodafone.com>
2025-04-18 21:44:56 +0200

    fix the exception file

---
+++ src/main/kotlin/model/Exceptions.kt
@@ -0,0 +1,31 @@
+package model
+
+open class AppException(message: String) : Exception(message)
+
+class WrongInputException : AppException("wrong Input")
+
+class NoEasyMealsFoundException : AppException("No easy meals found matching the criteria.")
+
+class NoMealsFoundException : AppException("No meals found!")
+
+class EmptyListException : AppException("list is empty")
+
+class EmptyHighCalorieListException : AppException("there is no high calories meals in list")
+
+class RichMaxAttemptException(exceptionMessage: String?) : Exception(exceptionMessage)
+
+class EmptySearchByDateListException : AppException("there is no meals in this date list")
+
+class InvalidDateFormatException : AppException("Invalid Date Format")
+
+class InvalidYearException : AppException("Invalid Year Input")
+
+class InvalidMonthException : AppException("Invalid Month Input")
+
+class InvalidDayException : AppException("Invalid Day Input")
+
+class InvalidIdException : AppException("Invalid ID Input")
+
+class NoIraqiMealsFoundException : AppException("No Iraqi meals found")
+
+class CountryMealsFetchException : AppException("Wrong country name")
