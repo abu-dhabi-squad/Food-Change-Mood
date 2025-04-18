f073c437f89baeaec9feb736948a5cf7cb940fd5
Eman Abobakr <eman.abobakr@vodafone.com>
2025-04-18 22:45:18 +0200

    split isKetoDietMeal function

--- src/main/kotlin/logic/usecase/GetRandomKetoDietMealsUseCase.kt
+++ src/main/kotlin/logic/usecase/GetRandomKetoDietMealsUseCase.kt
@@ -16,25 +16,40 @@ class GetRandomKetoDietMealsUseCase(private val foodRepository: FoodRepository) 
     private fun isKetoDietMeal(
         nutrition: Nutrition
     ): Boolean {
-        val calories = nutrition.calories
-        val carbohydrates = nutrition.carbohydrates
-        val fatCalories = nutrition.totalFat * FAT_CALORIES_BER_GRAM
-        val proteinCalories = nutrition.protein * PROTEIN_CALORIES_BER_GRAM
-        val carbCalories = carbohydrates * CARB_CALORIES_BER_GRAM
+        if (!isValidSugarAmount(nutrition.sugar))
+            return false
 
-        val fatPercentage = fatCalories / calories * 100
-        val proteinPercentage = proteinCalories / calories * 100
-        val carbPercentage = carbCalories / calories * 100
-        val saturatedFitPercentage = nutrition.saturated / nutrition.totalFat * 100
-        val isKeto = fatPercentage >= 60f &&
-                     proteinPercentage <= 25f &&
-                     carbPercentage <= 10f &&
-                     carbohydrates <= 20f &&
-                     saturatedFitPercentage in 20f .. 25f
-
-        if (nutrition.sugar > 5f) return false
+        val isKeto = isValidFatPercentage(nutrition.totalFat, nutrition.calories) &&
+                isValidProteinPercentage(nutrition.protein, nutrition.calories) &&
+                isValidCarbPercentage(nutrition.carbohydrates, nutrition.calories) &&
+                isValidSaturatedFatPercentage(nutrition.saturated, nutrition.totalFat)
 
         return isKeto
+    }
+
+    private fun isValidSugarAmount(sugar: Float) = sugar < 5f
+
+    private fun isValidFatPercentage(totalFat: Float, calories: Float): Boolean {
+        val fatCalories = totalFat * FAT_CALORIES_BER_GRAM
+        val fatPercentage = fatCalories / calories * 100
+        return fatPercentage >= 60f
+    }
+
+    private fun isValidProteinPercentage(protein: Float, calories: Float): Boolean {
+        val proteinCalories = protein * PROTEIN_CALORIES_BER_GRAM
+        val proteinPercentage = proteinCalories / calories * 100
+        return proteinPercentage <= 25f
+    }
+
+    private fun isValidCarbPercentage(carb: Float, calories: Float): Boolean {
+        val carbCalories = carb * CARB_CALORIES_BER_GRAM
+        val carbPercentage = carbCalories / calories * 100
+        return carbPercentage <= 10f
+    }
+
+    private fun isValidSaturatedFatPercentage(saturated: Float, totalFat: Float): Boolean {
+        val saturatedFatPercentage = saturated / totalFat * 100
+        return saturatedFatPercentage in 20f..25f
     }
 
     companion object {
