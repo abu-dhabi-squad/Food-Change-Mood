ade32d096ea304323a87c83274aac796b354cfe5
Eman Abobakr <eman.abobakr@vodafone.com>
2025-04-18 23:01:59 +0200

    format the utils file

--- src/main/kotlin/presentation/utils.kt
+++ src/main/kotlin/presentation/utils.kt
@@ -1,15 +1,20 @@
 package presentation
 
-import model.Food
 import model.WrongInputException
 
 
-fun isTheMealLikable():Boolean{
+fun isTheMealLikable(): Boolean {
     println("Do you like it? {y/n}")
     readLine().let {
-        when{
-            it.equals("y",true) -> { return true }
-            it.equals("n",true) -> { return false }
+        when {
+            it.equals("y", true) -> {
+                return true
+            }
+
+            it.equals("n", true) -> {
+                return false
+            }
+
             else -> {
                 throw WrongInputException()
             }
