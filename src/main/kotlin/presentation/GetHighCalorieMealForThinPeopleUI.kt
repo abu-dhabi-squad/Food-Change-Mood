df741538d8d7ffddba744e00826e8622c59bb2e6
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-22 16:10:41 +0200

    add depend injection for printer

--- src/main/kotlin/presentation/GetHighCalorieMealForThinPeopleUI.kt
+++ src/main/kotlin/presentation/GetHighCalorieMealForThinPeopleUI.kt
@@ -1,10 +1,12 @@
 package presentation
 
 import logic.usecase.GetMealForThinPeopleUseCase
+import presentation.ui_io.Printer
 import util.showDetails
 
 class GetHighCalorieMealForThinPeopleUI(
     private val getMealForThinPeopleUseCase: GetMealForThinPeopleUseCase,
+    private val printer: Printer
 ) : ChangeFoodMoodLauncher {
     override fun launchUI() {
         val shownSet = mutableSetOf<Int>()
@@ -15,16 +17,15 @@ class GetHighCalorieMealForThinPeopleUI(
         try {
             getMealForThinPeopleUseCase.getMeal(shownSet).also { suggestMeal ->
                 shownSet.add(suggestMeal.id)
-                println("Meal Name: " + suggestMeal.name + "\n")
-                println("Meal Description: " + suggestMeal.description + "\n")
-
+                printer.println("Meal Name: " + suggestMeal.name + "\n")
+                printer.println("Meal Description: " + suggestMeal.description + "\n")
                 when (isLikedMeal()) {
                     true -> suggestMeal.showDetails()
                     false -> getRandomHighCalorieMeal(shownSet)
                 }
             }
         } catch (e: Exception) {
-            println(e.message)
+            e.message?.let { printer.println(it) }
         }
     }
 
