4506cbf4702dc53fa51ba2730106229060cf25ba
noor serry <noor.serry.94@gmail.com>
2025-04-22 20:26:38 +0200

    refactor GuessFoodPreparationTimeUseCaseTest

--- src/test/kotlin/logic/usecase/GuessFoodPreparationTimeUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GuessFoodPreparationTimeUseCaseTest.kt
@@ -7,69 +7,93 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class GuessFoodPreparationTimeUseCaseTest{
+class GuessFoodPreparationTimeUseCaseTest {
 
-    private lateinit var guessFoodPreparationTimeUseCase : GuessFoodPreparationTimeUseCase
+    private lateinit var guessFoodPreparationTimeUseCase: GuessFoodPreparationTimeUseCase
+
     @BeforeEach
-    fun setup(){
+    fun setup() {
         guessFoodPreparationTimeUseCase = GuessFoodPreparationTimeUseCase()
     }
 
     @Test
-    fun `should return CORRECT when user guess equal the preparation time`(){
-
+    fun `should return CORRECT when user guess equal the preparation time`() {
+        // given
         val userGuess = 15
         val preparationTime = 15
         val attempts = 2
-
-      val result =   guessFoodPreparationTimeUseCase(userGuess = userGuess, preparationTime = preparationTime,attempts = attempts)
-
+        // when
+        val result = guessFoodPreparationTimeUseCase(
+            userGuess = userGuess,
+            preparationTime = preparationTime,
+            attempts = attempts
+        )
+        // then
         assertThat(result).isEqualTo(GuessPreparationTimeState.CORRECT)
     }
 
     @Test
-    fun `should return TOO_HIGH when user guess high than the preparation time`(){
+    fun `should return TOO_HIGH when user guess high than the preparation time`() {
+        // given
         val userGuess = 20
         val preparationTime = 15
         val attempts = 2
-
-        val result =   guessFoodPreparationTimeUseCase(userGuess = userGuess, preparationTime = preparationTime,attempts = attempts)
-
+        // when
+        val result = guessFoodPreparationTimeUseCase(
+            userGuess = userGuess,
+            preparationTime = preparationTime,
+            attempts = attempts
+        )
+        // then
         assertThat(result).isEqualTo(GuessPreparationTimeState.TOO_HIGH)
     }
 
     @Test
-    fun `should return TOO_LOW when user guess less than the preparation time`(){
+    fun `should return TOO_LOW when user guess less than the preparation time`() {
+        // given
         val userGuess = 10
         val preparationTime = 15
         val attempts = 2
-
-        val result =   guessFoodPreparationTimeUseCase(userGuess = userGuess, preparationTime = preparationTime,attempts = attempts)
-
+        // when
+        val result = guessFoodPreparationTimeUseCase(
+            userGuess = userGuess,
+            preparationTime = preparationTime,
+            attempts = attempts
+        )
+        // then
         assertThat(result).isEqualTo(GuessPreparationTimeState.TOO_LOW)
     }
 
     @Test
-    fun `should throw RichMaxAttemptException when rich max attempts with incorrect guess value`(){
+    fun `should throw RichMaxAttemptException when rich max attempts with incorrect guess value`() {
+        // given
         val userGuess = 10
         val preparationTime = 15
         val attempts = 3
-
-
-        assertThrows<RichMaxAttemptException>  {
-            guessFoodPreparationTimeUseCase(userGuess = userGuess, preparationTime = preparationTime,attempts = attempts)
+        // when && then
+        assertThrows<RichMaxAttemptException> {
+            guessFoodPreparationTimeUseCase(
+                userGuess = userGuess,
+                preparationTime = preparationTime,
+                attempts = attempts
+            )
         }
     }
 
 
     @Test
-    fun `guessFoodPreparationTimeUseCase should return CORRECT when rich max attempts with correct guess value`(){
+    fun `guessFoodPreparationTimeUseCase should return CORRECT when rich max attempts with correct guess value`() {
+        // given
         val userGuess = 15
         val preparationTime = 15
         val attempts = 3
-
-        val result =   guessFoodPreparationTimeUseCase(userGuess = userGuess, preparationTime = preparationTime,attempts = attempts)
-
+        // when &&
+        val result = guessFoodPreparationTimeUseCase(
+            userGuess = userGuess,
+            preparationTime = preparationTime,
+            attempts = attempts
+        )
+        // then
         assertThat(result).isEqualTo(GuessPreparationTimeState.CORRECT)
     }
 
