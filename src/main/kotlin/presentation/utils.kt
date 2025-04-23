2c2066e7b6e1b25a5b3e2e192fa46326b3917527
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-23 12:27:06 +0200

    remove unused imports

--- src/main/kotlin/presentation/utils.kt
+++ src/main/kotlin/presentation/utils.kt
@@ -1,9 +1,6 @@
 package presentation
 
-import model.Food
-import model.Nutrition
 import model.WrongInputException
-import java.time.LocalDate
 
 fun isLikedMeal(): Boolean {
     println("Do you like it? {y/n}")
