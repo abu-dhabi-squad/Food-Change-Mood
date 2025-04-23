fc15d75235d7632680e3b473adb2f4a2c3b163d4
Elsayed Magdy <saikoo1239@gmail.com>
2025-04-23 22:46:18 +0200

    merge dev

--- src/test/kotlin/logic/usecase/GetRandomPotatoesMealsUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GetRandomPotatoesMealsUseCaseTest.kt
@@ -37,28 +37,28 @@ class GetRandomPotatoesMealsUseCaseTest {
     fun `should return ten meals when food repository returns success  if more are available`() {
         // Given
         val potatoMeals = listOf(
-            createFood(
+            createPotatoMeal(
                 "tourtiere french canadian meat pie",
                 ingredients = listOf("potatoes", "pickel")
             ),
-            createFood("potatoes with green sauce", ingredients = listOf("potatoes", "pickel")),
-            createFood(
+            createPotatoMeal("potatoes with green sauce", ingredients = listOf("potatoes", "pickel")),
+            createPotatoMeal(
                 "cottage pie with kumara topping",
                 ingredients = listOf("potatoes", "pickel")
             ),
-            createFood(
+            createPotatoMeal(
                 "poor man s casserole potato egg bacon casserole",
                 ingredients = listOf("potatoes", "pickel")
             ),
-            createFood("swiss steak supper crock pot", ingredients = listOf("potatoes", "pickel")),
-            createFood(
+            createPotatoMeal("swiss steak supper crock pot", ingredients = listOf("potatoes", "pickel")),
+            createPotatoMeal(
                 "delicious oven baked beef stew",
                 ingredients = listOf("potatoes", "pickel")
             ),
-            createFood("gauranga potatoes", ingredients = listOf("potatoes", "pickel")),
-            createFood("cilantro potato soup", ingredients = listOf("potatoes", "pickel")),
-            createFood("ham cheese baked potatoes", ingredients = listOf("potatoes", "pickel")),
-            createFood("sweet potato soup world class", ingredients = listOf("potatoes", "pickel"))
+            createPotatoMeal("gauranga potatoes", ingredients = listOf("potatoes", "pickel")),
+            createPotatoMeal("cilantro potato soup", ingredients = listOf("potatoes", "pickel")),
+            createPotatoMeal("ham cheese baked potatoes", ingredients = listOf("potatoes", "pickel")),
+            createPotatoMeal("sweet potato soup world class", ingredients = listOf("potatoes", "pickel"))
         )
 
         every { foodRepository.getFoods() } returns Result.success(potatoMeals)
@@ -75,9 +75,9 @@ class GetRandomPotatoesMealsUseCaseTest {
     fun `should skip meals with blank or null names`() {
         // Given
         val potatoMeals = listOf(
-            createFood(null, ingredients = listOf("potatoes")),
-            createFood("", ingredients = listOf("potatoes")),
-            createFood(" ", ingredients = listOf("potatoes"))
+            createPotatoMeal(null, ingredients = listOf("potatoes")),
+            createPotatoMeal("", ingredients = listOf("potatoes")),
+            createPotatoMeal(" ", ingredients = listOf("potatoes"))
         )
 
         every { foodRepository.getFoods() } returns Result.success(potatoMeals)
@@ -94,8 +94,8 @@ class GetRandomPotatoesMealsUseCaseTest {
     fun `should skip meals that do not contain potatoes in ingredients`() {
         // Given
         val potatoMeals = listOf(
-            createFood("Rice Dish", ingredients = listOf("rice", "chicken")),
-            createFood("Salad", ingredients = listOf("lettuce", "tomato"))
+            createPotatoMeal("Rice Dish", ingredients = listOf("rice", "chicken")),
+            createPotatoMeal("Salad", ingredients = listOf("lettuce", "tomato"))
         )
 
         every { foodRepository.getFoods() } returns Result.success(potatoMeals)
@@ -109,11 +109,11 @@ class GetRandomPotatoesMealsUseCaseTest {
     fun `should return only valid meals with name and potatoes ingredient`() {
         // Given
         val meals = listOf(
-            createFood("Potato Pie", ingredients = listOf("potatoes")),
-            createFood(null, ingredients = listOf("potatoes")),
-            createFood("Potato Salad", ingredients = listOf("lettuce", "tomato")),
-            createFood(" ", ingredients = listOf("potatoes")),
-            createFood("Mashed Potatoes", ingredients = listOf("potatoes", "butter"))
+            createPotatoMeal("Potato Pie", ingredients = listOf("potatoes")),
+            createPotatoMeal(null, ingredients = listOf("potatoes")),
+            createPotatoMeal("Potato Salad", ingredients = listOf("lettuce", "tomato")),
+            createPotatoMeal(" ", ingredients = listOf("potatoes")),
+            createPotatoMeal("Mashed Potatoes", ingredients = listOf("potatoes", "butter"))
         )
 
         every { foodRepository.getFoods() } returns Result.success(meals)
