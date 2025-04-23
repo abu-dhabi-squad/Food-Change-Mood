1886dbdc00f1ca46d250d043a33ae6afaa714245
Elsayed Magdy <saikoo1239@gmail.com>
2025-04-23 14:42:59 +0200

    create get random potatoes meals use case test

---
+++ src/test/kotlin/logic/usecase/CreateFoodHelper.kt
@@ -0,0 +1,23 @@
+package logic.usecase
+
+
+import model.Food
+import model.Nutrition
+
+fun createFood(
+    name: String?,
+    description: String?,
+    tags: List<String>
+): Food {
+    return Food(
+        id = 0,
+        name = name,
+        minutes = 0,
+        submittedDate = null,
+        tags = tags,
+        nutrition = Nutrition(0.0f, 0.0f, 0.0f, 0.0f, 0.0f,0.0f,0.0f), // default empty nutrition
+        steps = emptyList(),
+        description = description,
+        ingredients = emptyList()
+    )
+}
