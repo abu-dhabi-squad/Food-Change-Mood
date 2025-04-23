eff1b19ed5ad9e244e083e5ebe2b3d76f141ae98
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-04-23 12:27:34 +0300

    add necessary setup for Easy Food Suggestion Game Use Case Test

--- src/test/kotlin/logic/usecase/EasyFoodSuggestionGameUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/EasyFoodSuggestionGameUseCaseTest.kt
@@ -1,8 +1,20 @@
 package logic.usecase
 
+import io.mockk.mockk
+import logic.repository.FoodRepository
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 
 class EasyFoodSuggestionGameUseCaseTest {
+
+    private lateinit var foodRepository: FoodRepository
+    private lateinit var easyFoodSuggestionGameUseCase: EasyFoodSuggestionGameUseCase
+
+    @BeforeEach
+    fun setup(){
+        foodRepository = mockk()
+        easyFoodSuggestionGameUseCase = EasyFoodSuggestionGameUseCase(foodRepository)
+    }
 
 
 }
