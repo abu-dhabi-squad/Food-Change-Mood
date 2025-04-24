33d662e79fd18837d205f8c4ba8aff52631a06fa
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-24 13:43:08 +0200

    remove unused imports

--- src/main/kotlin/util/DateValidationInterface.kt
+++ src/main/kotlin/util/DateValidationInterface.kt
@@ -1,7 +1,5 @@
 package util
 
-import java.time.LocalDate
-
 interface DateValidationInterface {
     fun isValidDate(date: String):Boolean
 }
--- src/test/kotlin/util/DateParserImplementationTest.kt
+++ src/test/kotlin/util/DateParserImplementationTest.kt
@@ -1,7 +1,6 @@
 package util
 
 import com.google.common.truth.Truth
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
