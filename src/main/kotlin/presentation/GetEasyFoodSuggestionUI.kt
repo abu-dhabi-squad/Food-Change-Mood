f77b4345825ecaaab8635866477bf6839366314e
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-04-24 03:29:34 +0300

    replaced println in GetEasyFoodSuggestionUI with injected Printer

--- src/main/kotlin/presentation/GetEasyFoodSuggestionUI.kt
+++ src/main/kotlin/presentation/GetEasyFoodSuggestionUI.kt
@@ -3,35 +3,36 @@ package presentation
 import logic.usecase.EasyFoodSuggestionGameUseCase
 import model.AppException
 import model.Food
+import presentation.ui_io.Printer
 
 class GetEasyFoodSuggestionUI(
-    private val easyFoodSuggestionGameUseCase: EasyFoodSuggestionGameUseCase
+    private val easyFoodSuggestionGameUseCase: EasyFoodSuggestionGameUseCase,
+    private val printer: Printer
 ) : ChangeFoodMoodLauncher {
 
     override fun launchUI() {
         try {
             val meals = easyFoodSuggestionGameUseCase.suggestRandomEasyMeals()
             if (meals.isEmpty()) {
-                println("No easy meals found. Please try again.")
+                printer.displayLn(EMPTY_INPUT_MESSAGE)
                 return
             }
-            println("\n  Here are 10 easy meals for you:")
+            printer.displayLn(START_MESSAGE)
             printMealDetails(meals)
-            println("--------------------------------------------------\n")
+            printer.displayLn(MEAL_SEPARATOR)
         } catch (exception: AppException) {
-            println(exception.message)
+            printer.displayLn(exception.message ?: ERROR_MESSAGE)
         }
     }
 
     private fun printMealDetails(meals: List<Food>) {
         meals.forEachIndexed { index, meal ->
-            println("--------------------------------------------------")
-            println("${index + 1}. ${meal.name ?: "Unnamed Meal"}")
-            println("Prepared Time: ${meal.minutes} minutes")
-            println("Description: ${meal.description ?: "No description available"}")
-            println("Ingredients: ${meal.ingredients.joinToString(", ")}")
-            println("Steps: ${meal.steps.size} steps")
-            println("Nutrition: Calories = ${meal.nutrition.calories}, Protein = ${meal.nutrition.protein}g")
+            println(MEAL_SEPARATOR)
+            printer.displayLn("${index + 1}. ${meal.name ?: UNNAMED_MEAL}")
+            printer.displayLn("$PREP_TIME_LABEL: ${meal.minutes} $MINUTES")
+            printer.displayLn("$DESCRIPTION_LABEL: ${meal.description ?: NO_DESCRIPTION}")
+            printer.displayLn("$INGREDIENTS_LABEL: ${meal.ingredients.joinToString(", ")}")
+            printer.displayLn("$STEPS_LABEL: ${meal.steps.size} $STEPS")
         }
     }
 }
