ed83b01b202e0bef72904399d5cb94f430f3ebdc
Nour AbuFayyad <nourabufayyad@gmail.com>
2025-04-24 05:20:10 +0300

    moved buildPrefixTable and isContainsPattern from GetMealBySearchNameUseCase   to a new utility object KMPMatcher

---
+++ src/main/kotlin/util/KMPMatcher.kt
@@ -0,0 +1,49 @@
+package util
+
+object KMPMatcher {
+
+    fun isContainsPattern(text: String, pattern: String): Boolean {
+        if (pattern.isEmpty()) return true
+
+        val prefixTable = buildPrefixTable(pattern)
+        var textIndex = 0
+        var patternIndex = 0
+
+        while (textIndex < text.length) {
+            if (text[textIndex] == pattern[patternIndex]) {
+                textIndex++
+                patternIndex++
+                if (patternIndex == pattern.length) return true
+            } else if (patternIndex != 0) {
+                patternIndex = prefixTable[patternIndex - 1]
+            } else {
+                textIndex++
+            }
+        }
+        return false
+    }
+
+    private fun buildPrefixTable(pattern: String): IntArray {
+        val table = IntArray(pattern.length)
+        var prefixLength = 0
+        var indexOfCurrentChar = 1
+
+        while (indexOfCurrentChar < pattern.length) {
+            when {
+                pattern[indexOfCurrentChar] == pattern[prefixLength] -> {
+                    prefixLength++
+                    table[indexOfCurrentChar] = prefixLength
+                    indexOfCurrentChar++
+                }
+                prefixLength != 0 -> {
+                    prefixLength = table[prefixLength - 1]
+                }
+                else -> {
+                    table[indexOfCurrentChar] = 0
+                    indexOfCurrentChar++
+                }
+            }
+        }
+        return table
+    }
+}
---
+++ src/test/kotlin/logic/usecase/GetMealBySearchNameUseCaseTest.kt
@@ -0,0 +1,7 @@
+package logic.usecase
+
+import org.junit.jupiter.api.Assertions.*
+class GetMealBySearchNameUseCaseTest {
+
+  
+}
